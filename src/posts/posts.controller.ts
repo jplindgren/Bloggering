ff63fbefe12b3cfa1a354fe49806b0c3a1dfa398
Joao Lindgren <joaopozo@gmail.com>
2020-04-07 19:36:48 -0300

    Add feature to create and get posts

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,21 +1,34 @@
-import { Controller, Post, Body, Get, Param, Patch, UseGuards, HttpCode } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Patch, UseGuards, HttpCode, Req } from '@nestjs/common';
 import { ApiResponse } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
-import { Post as BlogPost, SEED_BLOG_POSTS } from './post.entity'
+import { Post as BlogPost, PostDto } from './post.entity'
+import { PostsService } from './posts.service';
+import { identity } from 'rxjs';
 
 @Controller('posts')
-export class PostsController {
+export class PostsController {    
+    constructor(private readonly postsService:PostsService) {         
+    }
+
     @Get()    
     @ApiResponse({ type: BlogPost, status: 200, isArray: true }) // for Swagger documentation: API returns an array of Post models
     findAll(): Array<BlogPost> {
-        return SEED_BLOG_POSTS;
+        return this.postsService.getPosts();
+    }
+
+    @Get(':id')
+    @ApiResponse({ type: BlogPost, status: 200 })
+    find(@Param('id') postId: number): BlogPost {
+        return this.postsService.getSinglePost(postId);
     }
 
     @Post()
     @UseGuards(AuthGuard('jwt'))
     @HttpCode(200)
     @ApiResponse({ status: 200 }) 
-    createPost(@Body('name') name: string) {
-        return "Post created";
+    create(@Body() newPost: PostDto, @Req() req) {        
+        const userId = req['user'].thirdPartyId;
+        const newPostId = this.postsService.createPost(newPost.title, userId ,newPost.content);        
+        return {id: newPostId};
     }
 }
