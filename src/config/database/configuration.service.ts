3cf1495bd6fca89dba205bbe616cf9f1041f4e91
Joao Lindgren <joaopozo@gmail.com>
2020-04-12 20:42:41 -0300

    Add posts as a real entity saved in database. Add relationship to user as author.

--- src/config/database/configuration.service.ts
+++ src/config/database/configuration.service.ts
@@ -3,23 +3,24 @@ import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 
 @Injectable()
-export class OrmConfigService implements TypeOrmOptionsFactory{
-  constructor(private configService: ConfigService) {}
-  
-  createTypeOrmOptions(): TypeOrmModuleOptions {
-    const type: any = this.configService.get<string>('orm.type');
-    return {
-        type,
-        host: this.configService.get<string>('orm.host'),
-        username: this.configService.get<string>('orm.username'),
-        password: this.configService.get<string>('orm.password'),
-        database: this.configService.get<string>('orm.database'),
-        port: this.configService.get<number>('orm.port'),
-        logging: this.configService.get<boolean>('orm.logging'),
-        entities: this.configService.get<string[]>('orm.entities'),
-        migrations: this.configService.get<string[]>('orm.migrations'),
-        migrationsRun: this.configService.get<boolean>('orm.migrationsRun'),
-        cli: this.configService.get<any>('orm.cli')
-      };
-  } 
+export class OrmConfigService implements TypeOrmOptionsFactory {
+    constructor(private configService: ConfigService) {}
+
+    createTypeOrmOptions(): TypeOrmModuleOptions {
+        const type: any = this.configService.get<string>('orm.type');
+        return {
+            type,
+            host: this.configService.get<string>('orm.host'),
+            username: this.configService.get<string>('orm.username'),
+            password: this.configService.get<string>('orm.password'),
+            database: this.configService.get<string>('orm.database'),
+            port: this.configService.get<number>('orm.port'),
+            logging: this.configService.get<boolean>('orm.logging'),
+            entities: this.configService.get<string[]>('orm.entities'),
+            migrations: this.configService.get<string[]>('orm.migrations'),
+            migrationsRun: this.configService.get<boolean>('orm.migrationsRun'),
+            synchronize: this.configService.get<boolean>('orm.sincronize'),
+            cli: this.configService.get<any>('orm.cli'),
+        };
+    }
 }
