394934a33b46a0b2820faa4a642989ade5cca554
Joao Lindgren <joaopozo@gmail.com>
2020-05-15 18:43:31 -0300

    Add Elastic Search for indexing posts.

---
+++ src/config/elasticsearch/configuration.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ElasticsearchOptionsFactory, ElasticsearchModuleOptions } from '@nestjs/elasticsearch';
+
+@Injectable()
+export class ElasticsearchConfigService implements ElasticsearchOptionsFactory {
+    constructor(private configService: ConfigService) { }
+
+    createElasticsearchOptions(): ElasticsearchModuleOptions {
+        const node = `http://${this.configService.get<string>('es.host')}:${this.configService.get<string>('es.port')}`;
+        return {
+            node
+        };
+    }
+}
